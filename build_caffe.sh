5e404f654c2e72bf47c897fbd73e7f03f99262ed
xxx <kamei@urobot.co.jp>
2016-09-16 13:17:45 +0900

    update build_caffe.sh

--- build_caffe.sh
+++ build_caffe.sh
@@ -13,9 +13,10 @@ cd "${WD}"
 TARGET_ABI=${2:-"armeabi-v7a,arm64-v8a,x86,x86_64"}
 N_JOBS=${3:-"16"}
 TARGET_API_LEVELS="${1:-"14,21"}"
-
+TARGET_API_LEVELS=(`echo $TARGET_API_LEVELS | tr -s ',' ' '`)
 for TARGET_API_LEVEL in ${TARGET_API_LEVELS[@]}; do
-    export EXPORT_TARGET_API_LEVEL=TARGET_API_LEVEL
+    export EXPORT_TARGET_API_LEVEL=${TARGET_API_LEVEL}
+
     echo "---------------------------------------"
     echo "${0}"
     echo "EXPORT_TARGET_API_LEVEL:${EXPORT_TARGET_API_LEVEL}"
